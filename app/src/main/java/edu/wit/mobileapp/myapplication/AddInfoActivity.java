91d47fc8a790b102d7353c2f2430d224dc8776a8
Justin Nguyen <nguyenj32@wit.edu>
2020-03-12 14:40:18 -0400

    Added colors, arranged layout, added date picker

--- app/src/main/java/edu/wit/mobileapp/myapplication/AddInfoActivity.java
+++ app/src/main/java/edu/wit/mobileapp/myapplication/AddInfoActivity.java
@@ -1,11 +1,9 @@
 package edu.wit.mobileapp.myapplication;
 
-import androidx.annotation.IdRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -17,8 +15,9 @@ import com.google.android.material.chip.ChipGroup;
 
 import java.util.ArrayList;
 
-public class AddInfoActivity extends AppCompatActivity {
+public class AddInfoActivity extends AppCompatActivity implements View.OnClickListener {
     private EditText editText;
+    private Button next_btn;
     private Button button;
     private ChipGroup chipGroup;
 
@@ -31,32 +30,9 @@ public class AddInfoActivity extends AppCompatActivity {
         editText = findViewById(R.id.edit_text_add_chip);
         button = findViewById(R.id.btn_add_chip_class);
         chipGroup = findViewById(R.id.class_group);
-        Button next_btn = (Button)findViewById(R.id.buttonNext);
+        next_btn = findViewById(R.id.buttonNext);
 
-        // OnClickListener in next_btn to Bundle and Intent to AddAssignmentActivity
-        // This bundles the chip texts inputted from the user
-        next_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(AddInfoActivity.this, AddAssignmentActivity.class);
-
-                ChipGroup chipGroupClass = findViewById(R.id.class_group);
-
-                ArrayList<String > chipClassList = new ArrayList<String>();
-
-                for(int i = 0; i < chipGroupClass.getChildCount();i++){
-                    Chip chip = (Chip)chipGroup.getChildAt(i);
-                        Log.v("myApp",i + " "+ chip.getText().toString());
-                        chipClassList.add(chip.getText().toString());
-                }
-                Bundle bundle = new Bundle();
-                bundle.putStringArrayList("classList", chipClassList);
-                intent.putExtras(bundle);
-                startActivity(intent);
-            }
-
-        });
+        next_btn.setOnClickListener(this);
     }
 
     public void btnClickAddClass(View view){
@@ -75,8 +51,31 @@ public class AddInfoActivity extends AppCompatActivity {
                 chipGroup.removeView(chip);
             }
         });
-
         chipGroup.addView(chip);
         editText.setText("");
+    }
+
+    @Override
+    public void onClick(View v) {
+        // OnClickListener in next_btn to Bundle and Intent to AddAssignmentActivity
+        // This bundles the chip texts inputted from the user
+        if(v == next_btn) {
+            Intent intent = new Intent();
+            intent.setClass(AddInfoActivity.this, AddAssignmentActivity.class);
+
+            ChipGroup chipGroupClass = findViewById(R.id.class_group);
+
+            ArrayList<String> chipClassList = new ArrayList<String>();
+
+            for (int i = 0; i < chipGroupClass.getChildCount(); i++) {
+                Chip chip = (Chip) chipGroup.getChildAt(i);
+                Log.v("myApp", i + " " + chip.getText().toString());
+                chipClassList.add(chip.getText().toString());
+            }
+            Bundle bundle = new Bundle();
+            bundle.putStringArrayList("classList", chipClassList);
+            intent.putExtras(bundle);
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/edu/wit/mobileapp/myapplication/CardViewAssignmentActivity.java
+++ app/src/main/java/edu/wit/mobileapp/myapplication/CardViewAssignmentActivity.java
@@ -3,6 +3,7 @@ package edu.wit.mobileapp.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,8 +12,9 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CardViewAssignmentActivity extends AppCompatActivity {
+public class CardViewAssignmentActivity extends AppCompatActivity implements View.OnClickListener{
     private FloatingActionButton mAddFab;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +23,7 @@ public class CardViewAssignmentActivity extends AppCompatActivity {
         // Linked views to their ID
         mAddFab = findViewById(R.id.floatingActionButton);
 
+        mAddFab.setOnClickListener(this);
 
         Bundle bundle = this.getIntent().getExtras();
         String title = bundle.getString("title");
@@ -38,18 +41,26 @@ public class CardViewAssignmentActivity extends AppCompatActivity {
         item1.priority = priority;
         list.add(item1);
 
-        mAddFab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(CardViewAssignmentActivity.this, AddAssignmentActivity.class);
-                startActivity(intent);
-            }
-        });
+        CardItem item2 = new CardItem();
+        item2.title = title;
+        item2.date = due;
+        item2.assignment = assignment;
+        item2.classes = classes;
+        item2.priority = priority;
+        list.add(item2);
 
         CardItemAdapter adapter;
         adapter = new CardItemAdapter(this, 0, list);
         ListView cardView = (ListView) findViewById(R.id.CardView);
         cardView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mAddFab) {
+            Intent intent = new Intent();
+            intent.setClass(CardViewAssignmentActivity.this, AddAssignmentActivity.class);
+            startActivity(intent);
+        }
     }
 }
