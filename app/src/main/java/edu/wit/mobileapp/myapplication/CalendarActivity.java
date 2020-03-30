40f5ad2a2b7d686f62f448e2d3b93d26fff71d46
Justin Nguyen <nguyenj32@wit.edu>
2020-03-30 18:50:17 -0400

    Added Bottom NavBar, Working on Overflow Menu on Cards

---
+++ app/src/main/java/edu/wit/mobileapp/myapplication/CalendarActivity.java
@@ -0,0 +1,43 @@
+package edu.wit.mobileapp.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class CalendarActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calendar_layout);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.calendar);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.calendar:
+                        startActivity(new Intent(getApplicationContext(), CalendarActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.today:
+                        startActivity(new Intent(getApplicationContext(), CardViewAssignmentActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.data:
+                        startActivity(new Intent(getApplicationContext(), DataActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+}
