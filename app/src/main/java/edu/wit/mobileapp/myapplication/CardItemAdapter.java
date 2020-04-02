6ccd5460ac5f3e98f5e6ef95c2fab21922bf8291
percontim <percontim@wit.edu>
2020-04-02 16:00:42 -0400

    Added Text to speech for individual assignments

--- app/src/main/java/edu/wit/mobileapp/myapplication/CardItemAdapter.java
+++ app/src/main/java/edu/wit/mobileapp/myapplication/CardItemAdapter.java
@@ -1,6 +1,7 @@
 package edu.wit.mobileapp.myapplication;
 
 import android.content.Context;
+import android.speech.tts.TextToSpeech;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,9 +10,16 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.util.List;
+import java.util.Locale;
 
 public class CardItemAdapter extends ArrayAdapter<CardItem> {
     private LayoutInflater mInflater;
+    public TextToSpeech toSpeech;
+    public String cardTitle;
+    public String cardPriority;
+    public String cardClasses;
+    public String cardDate;
+    public String cardAssignmentType;
 
     public CardItemAdapter(Context context, int rid, List<CardItem> list) {
         super(context, rid, list);
@@ -25,23 +33,47 @@ public class CardItemAdapter extends ArrayAdapter<CardItem> {
 
         TextView title;
         title = (TextView) view.findViewById(R.id.title);
-        title.setText(item.title);
+        cardTitle = item.title;
+        title.setText(cardTitle);
 
         TextView date;
         date = (TextView) view.findViewById(R.id.date);
-        date.setText(item.date);
+        cardDate = item.date;
+        date.setText(cardDate);
 
         TextView assignment;
         assignment = (TextView) view.findViewById(R.id.assignment);
-        assignment.setText(item.assignmentType);
+        cardAssignmentType = item.assignmentType;
+        assignment.setText(cardAssignmentType);
 
         TextView classes;
         classes = (TextView) view.findViewById(R.id.classes);
-        classes.setText(item.classes);
+        cardClasses = item.classes;
+        classes.setText(cardClasses);
 
         TextView priority;
         priority = (TextView) view.findViewById(R.id.priority);
-        priority.setText(item.priority);
+        cardPriority = item.priority;
+        priority.setText(cardPriority);
+
+        ImageButton speechButton;
+        speechButton = (ImageButton) view.findViewById(R.id.talkButton);
+        toSpeech = new TextToSpeech(getContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR)
+                    toSpeech.setLanguage(Locale.US);
+            }
+        });
+        speechButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String speechString = cardTitle + ", " + cardAssignmentType + ", " +
+                        " due on " + cardDate + " for " + cardClasses +
+                        ". Priority is " + cardPriority;
+                toSpeech.speak(speechString, TextToSpeech.QUEUE_FLUSH, null, null);
+            }
+        });
 
         return view;
     }
